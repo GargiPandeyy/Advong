b5f864be6fe988d6fb00fe63bf0aba03a71ff021
GargiPandeyy <computercomcon@gmail.com>
2025-10-27 16:05:31 +0530

    add game state

---
+++ js/game.js
@@ -0,0 +1,15 @@
+const gameState = {
+    currentMode: null,
+    songs: [],
+    currentSongIndex: 0,
+    currentBlankIndex: 0,
+    score: 0,
+    hintsRemaining: 2,
+    skipsRemaining: 1,
+    hintsUsed: 0,
+    skipsUsed: 0,
+    correctAnswers: 0,
+    totalBlanks: 0,
+    hintUsedOnCurrentBlank: false,
+    unlockedModes: null
+};
